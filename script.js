c0cdfc6116c46be8146632df0f74e84a94ee8dc9
Thais Silva <thais_ap@live.com>
2023-02-25 16:31:07 -0300

    fixing the structure

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
 let arrow = document.querySelectorAll('.summary__arrow')
+let summary = document.querySelectorAll('.detail')
 
+summaryArr = Array.from(summary)
+arrowArr = Array.from(arrow)
 
-
-for (let i = 0; i < arrow.length; i++) {
-    arrow[i].addEventListener('click', rotate)
+for (let i = 0; i < summaryArr.length; i++) {
+    summaryArr[i].addEventListener('click', rotate)
 }
 
 function rotate(e){
-    e.target.classList.toggle('active')   
+    e.target.classList.toggle('active')
 }
-
-
-
-
-/*for (let i = 0; i < arrow.length; i++) {
-    document.body.addEventListener('click', function (e) {
-        if (e.target == arrow[i]) {
-            arrow[i].classList.toggle('active');
-        } else {
-            arrow[i].classList.remove('active')
-        }
-    })
-    
-}*/
 
 
 
